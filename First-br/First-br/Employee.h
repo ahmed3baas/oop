0bba3b193bc3edb970c1a47abeadbd8133759988
ahmed3baas <ahmed.3baas@gmail.com>
2020-09-26 02:59:21 -0700

    created class employee

---
+++ First-br/First-br/Employee.h
@@ -0,0 +1,22 @@
+#include <string>
+using namespace std;
+#pragma once
+class Employee
+{
+private:
+	string name;
+	double salary;
+	int age, departmentId;
+public:
+	void setName(string n);
+	void setSalary(double s);
+	void setDepartmentId(int id);
+
+	string getName();
+	double getSalary();
+	int getAge();
+	int getDepartmentId();
+	
+	Employee(string n,double s,int a,int id);
+};
+
